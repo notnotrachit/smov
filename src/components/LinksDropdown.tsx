3753602ac1159207f00ec9096353631308726470
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2025-02-13 21:31:24 +0530

    rename

--- src/components/LinksDropdown.tsx
+++ src/components/LinksDropdown.tsx
@@ -161,11 +161,6 @@ export function LinksDropdown(props: { children: React.ReactNode }) {
           <Divider />
           <div className="my-4 flex justify-center items-center gap-4">
             <CircleDropdownLink
-              href={conf().DISCORD_LINK}
-              icon={Icons.DISCORD}
-            />
-            <CircleDropdownLink href={conf().GITHUB_LINK} icon={Icons.GITHUB} />
-            <CircleDropdownLink
               href={conf().TWITTER_LINK}
               icon={Icons.TWITTER}
             />
